512e184beef4512dcda5a9c10a4fb266e86f80b9
VeryGreatFrog <bvl46z@gmail.com>
2022-09-07 23:59:12 +0200

    Revert "working version" And hope it works this tim!
    This reverts commit ccec1dacfe3930e61497112ac7b39f9a95faff3a.

--- src/map/components/LocationSelector.vue
+++ src/map/components/LocationSelector.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <p v-for="(value, key) in mapData['locations'][selectedMap.map]" @click="selectedLocations.locationSwitch(key.toString())">
+        <p  v-if="mapData" v-for="(value, key) in mapData['locations'][selectedMap.map]" @click="selectedLocations.locationSwitch(key.toString())">
             <button class="button locationBtn" :class="selectedLocations.list.includes(key.toString()) ? 'selected' : 'not-selected' && key.toString() != 'other' ? '' : 'hidden'">{{ key }}</button>
         </p>
     </div>
     <div>
         <p>Other</p>
-        <p v-for="(value, key) in mapData['locations'][selectedMap.map]['other']" @click="selectedLocations.locationSwitch(key.toString())">
+        <p  v-if="mapData" v-for="(value, key) in mapData['locations'][selectedMap.map]['other']" @click="selectedLocations.locationSwitch(key.toString())">
             <button class="button locationBtn" :class="selectedLocations.list.includes(key.toString()) ? 'selected' : 'not-selected'">{{ key }}</button>
         </p>
     </div>
@@ -28,15 +28,18 @@
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-import {mapData} from '../data';
+import {getMapData} from '../data';
 import {selectedLocations, selectedMap} from '../store';
 export default defineComponent({
     data() {
         return {
-            mapData: mapData,
+            mapData: null as null | any,
             selectedLocations,
             selectedMap,
         };
     },
+    async mounted() {
+        this.mapData = await getMapData()
+    }
 });
 </script>
