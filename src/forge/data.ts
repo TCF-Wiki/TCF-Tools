18f5d51b37e9b397c721901e8ae7f4d727638ebc
VeryGreatFrog <bvl46z@gmail.com>
2022-10-05 15:52:25 +0200

    Data

--- src/forge/data.ts
+++ src/forge/data.ts
@@ -1,1 +1,26 @@
-export {}
+async function get_ingot_data() {
+    const response = await fetch('https://raw.githubusercontent.com/TCF-Wiki/TCF-Information/main/AlienForgeIngotsRecipes.json', {});
+    const json = await response.json();
+
+    return json
+}
+
+export const ingotData = await get_ingot_data();
+
+async function get_settings_data() {
+    const response = await fetch('https://raw.githubusercontent.com/TCF-Wiki/TCF-Information/main/AlienForgeSettings.json', {});
+    const json = await response.json();
+
+    return json
+}
+
+export const settingData = await get_settings_data();
+
+async function get_recipe_data() {
+    const response = await fetch('https://raw.githubusercontent.com/TCF-Wiki/TCF-Information/main/RolledPerks.json', {});
+    const json = await response.json();
+
+    return json
+}
+
+export const recipeData = await get_recipe_data();
