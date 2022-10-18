d50bcab8c93f6aeb1f0084a0d6688c98d80fde40
VeryGreatFrog <bvl46z@gmail.com>
2022-10-18 16:53:53 +0200

    Revert "Merge branch 'forge-test' of https://github.com/TCF-Wiki/TCF-Apps into forge-test"
    This reverts commit dad85ba4adedfa99e87ae47d09379b5bfe94b4f6, reversing
    changes made to 4437f7841a258dcf57aa96058940e698770ed4ec.

--- src/forge/components/ClearButton.vue
+++ src/forge/components/ClearButton.vue
@@ -32,9 +32,27 @@ export default defineComponent({
     border-radius: 50%;
     text-align: center;
     cursor: pointer;
-    z-index: 2;
 
+}
+
+.clearBtn-Image {
     position: absolute;
+    transition: all .2s ease-out;
+    animation: pulse 5s infinite;
+
+    
+}
+
+img {
+    filter: hue-rotate(-70deg) !important;
+}
+
+.clearBtnHover-Image {
+    opacity: 0;
+    position: absolute;
+    transition: all .2s ease-out;
+    transform: scale(1.70);
+    left: .5%;
 }
 
 .forgeClear-Button:hover .clearBtnHover-Image {
@@ -42,23 +60,7 @@ export default defineComponent({
 }
 
 .forgeClear-Button:hover .clearBtn-Image {
-  display: none;
-}
-.clearBtnHover-Image {
-    position: absolute;
-    opacity: 0;
-    transition: all .2s ease-out;
-    transform: scale(1.80) translateY(-20%) translateX(-.5%);
-    left: 10.5%;
-    max-width: 75%;
-    bottom: -59%;
-}
-
-
-.clearBtn-Image {
-    position: absolute;
-    transition: all .2s ease-out;
-    animation: pulse 5s infinite;
+  opacity: 0;
 }
 
 </style>
