066d679d90d424d37a64c637ea7e15b4a70b5521
Stevnbak <kasper@stevnbak.dk>
2022-11-12 15:27:11 +0100

    Search hide fix

--- src/apps/map/components/ItemSearch.vue
+++ src/apps/map/components/ItemSearch.vue
@@ -1,53 +1,30 @@
 <template>
     <div class="outer-container">
         <form id="search" role="search" autocomplete="off">
-            <div 
-                class="search-container"
-            > 
-                <input 
-                    @input="matchInputs" 
-                    type="text" 
-                    id="search-input" 
-                    spellcheck="false" 
-                    v-model="searchInput"
-                    :class="{'apply-shake': shake}" 
-                    placeholder="Search for item..."
-                    aria-label="Input item you want to search"
+            <div class="search-container">
+                <input @input="matchInputs" type="text" id="search-input" spellcheck="false" v-model="searchInput" :class="{'apply-shake': shake}" placeholder="Search for item..." aria-label="Input item you want to search" ref="input" />
 
-                    ref="input"
-                >
-
-                <button 
-                type="submit" 
-                @click.prevent="addToSearchedList"
-                aria-label="Submit item search"
-                > 
-                <font-awesome-icon icon="fas fa-magnifying-glass" />
+                <button type="submit" @click.prevent="addToSearchedList" aria-label="Submit item search">
+                    <font-awesome-icon icon="fas fa-magnifying-glass" />
                 </button>
 
-                <button 
-                    type="reset" 
-                    @click="reset"
-                    aria-label="Clear item input"
-                > <font-awesome-icon icon="fas fa-circle-xmark" />
-                </button> 
+                <button type="reset" @click="reset" aria-label="Clear item input"><font-awesome-icon icon="fas fa-circle-xmark" /></button>
             </div>
-
         </form>
-            <div class="autocomplete" :class="{'has-items': matchingItems.length > 0}">
-                <TransitionGroup name="list" tag="ul">
-                    <li v-for="item, index in matchingItems" class="autocomplete-item" @click="setSearchItem(item)" :key="index">
-                        <span> {{ item }} </span>
-                        <img :src="'map-images/item-images/' + itemImage(item) + '.png'" class="item-image">
-                    </li>
-                </TransitionGroup>
-            </div>
+        <div class="autocomplete" :class="{'has-items': matchingItems.length > 0}">
+            <TransitionGroup name="list" tag="ul">
+                <li v-for="(item, index) in matchingItems" class="autocomplete-item" @click="setSearchItem(item)" :key="index">
+                    <span> {{ item }} </span>
+                    <img :src="'map-images/item-images/' + itemImage(item) + '.png'" class="item-image" />
+                </li>
+            </TransitionGroup>
+        </div>
 
         <div class="item-list">
             <div class="item-container">
                 <p v-for="item in selectedItems.get()" @click="selectedItems.remove(item)" v-if="Object.keys(items).length > 0">
                     <span :class="colourClassGiver(item)">
-                        {{  items[item]['name'] }} 
+                        {{ items[item]['name'] }}
                     </span>
                     <font-awesome-icon icon="fas fa-trash" />
                 </p>
@@ -59,18 +36,17 @@
             </div>
         </div>
     </div>
-
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { getMapData } from '../data';
-import { selectedItems } from '../store'
+import {defineComponent} from 'vue';
+import {getMapData} from '../data';
+import {selectedItems} from '../store';
 
 export default defineComponent({
     data() {
         return {
             selectedItems,
-            searchInput: "" as string,
+            searchInput: '' as string,
             matchingItems: [] as string[],
             data: {} as any,
             items: {} as any,
@@ -82,7 +58,7 @@ export default defineComponent({
     methods: {
         matchInputs: function () {
             const input = this.searchInput.toLowerCase();
-            if (input == "") {
+            if (input == '') {
                 this.matchingItems = [];
                 return;
             }
@@ -93,9 +69,9 @@ export default defineComponent({
             });
         },
         setSearchItem(item: string) {
-            this.searchInput = item
+            this.searchInput = item;
             this.matchingItems = [];
-            this.addToSearchedList()
+            this.addToSearchedList();
         },
         addToSearchedList() {
             // if (this.matchingItems.length == 1) {
@@ -109,9 +85,9 @@ export default defineComponent({
                 }, 820);
             }
             for (let item in this.items) {
-                if (this.items[item]["name"].toLowerCase() == input) {
+                if (this.items[item]['name'].toLowerCase() == input) {
                     selectedItems.add(item);
-                    this.searchInput = "";
+                    this.searchInput = '';
                     break;
                 }
             }
@@ -124,8 +100,7 @@ export default defineComponent({
             }
         },
         autoCompleteShower(): boolean {
-            if (document.querySelector("#search-input") === document.activeElement)
-                return true;
+            if (document.querySelector('#search-input') === document.activeElement) return true;
             return false;
         },
         reset() {
@@ -134,47 +109,41 @@ export default defineComponent({
         },
         colourClassGiver(item: string) {
             const itemData = this.items[item];
-            if (!itemData)
-                return "";
-            if (itemData["rarity"]) {
-                return itemData["rarity"].toLowerCase();
+            if (!itemData) return '';
+            if (itemData['rarity']) {
+                return itemData['rarity'].toLowerCase();
             }
         },
         itemImage(itemName: string): string {
-            let foundItem = "";
+            let foundItem = '';
             for (let item in this.items) {
-                if (this.items[item]["name"].toLowerCase() == itemName.toLowerCase()) {
+                if (this.items[item]['name'].toLowerCase() == itemName.toLowerCase()) {
                     foundItem = item;
                     break;
                 }
             }
-            let item = this.items[foundItem]["name"];
-            if (foundItem?.includes("Map01_KeyCard"))
-                return "Bright_Sands_Key_Card";
-            if (foundItem?.includes("Map02_KeyCard"))
-                return "Crescent_Falls_Key_Card";
-            if (foundItem?.includes("Map03_KeyCard"))
-                return "Tharis_Island_Key_Card";
-            if (foundItem?.includes("Flechette Gun"))
-                return "ASP Flechette Gun";
-            return item.replaceAll(" ", "_").replaceAll("#", "#");
-        }
+            let item = this.items[foundItem]['name'];
+            if (foundItem?.includes('Map01_KeyCard')) return 'Bright_Sands_Key_Card';
+            if (foundItem?.includes('Map02_KeyCard')) return 'Crescent_Falls_Key_Card';
+            if (foundItem?.includes('Map03_KeyCard')) return 'Tharis_Island_Key_Card';
+            if (foundItem?.includes('Flechette Gun')) return 'ASP Flechette Gun';
+            return item.replaceAll(' ', '_').replaceAll('#', '#');
+        },
     },
     async mounted() {
         this.data = await getMapData();
-        this.items = this.data["descriptions"];
+        this.items = this.data['descriptions'];
         let tempData = [];
         let tempLowerData = [];
         for (let x in this.items) {
             let data = this.items[x] as any;
-            tempData.push(data["name"]);
-            tempLowerData.push(data["name"].toLowerCase());
+            tempData.push(data['name']);
+            tempLowerData.push(data['name'].toLowerCase());
         }
         this.searchTerms = tempData;
         this.lowerCaseSearchTerms = tempLowerData;
     },
-})
-
+});
 </script>
 
 <style scoped>
@@ -200,7 +169,7 @@ h3 {
 form {
     display: flex;
     flex-direction: column;
-    gap: 1rem
+    gap: 1rem;
 }
 
 .search-container {
@@ -211,7 +180,7 @@ form {
 }
 
 .autocomplete {
-    margin-top: .5rem;
+    margin-top: 0.5rem;
     position: absolute;
     width: 100%;
     transform: translateY(3px);
@@ -220,17 +189,16 @@ form {
     overflow-x: hidden;
     background-image: linear-gradient(180deg, var(--rarity-color-common) 0%, var(--text-color-body-white-dark) 100%);
 
-
-
     overscroll-behavior: contain;
 
-    opacity: 1;
+    opacity: 0;
 }
 
 .autocomplete.has-items {
     border-radius: 5px;
 
     border: 2px solid var(--background-dark-color);
+    opacity: 1;
 }
 
 .autocomplete-item {
@@ -241,27 +209,24 @@ form {
     flex-direction: row;
     justify-content: space-between;
     z-index: 1;
-    padding: .3rem;
+    padding: 0.3rem;
 
-    transition: all .2s ease-in-out;
+    transition: all 0.2s ease-in-out;
 }
 
 .autocomplete-item:hover {
     background-color: var(--rarity-color-legendary);
 }
 
-.autocomplete-item >  span {
+.autocomplete-item > span {
     height: 2rem;
-    padding-top: .2rem;
-    padding-left: .1rem;
+    padding-top: 0.2rem;
+    padding-left: 0.1rem;
 }
 
 .item-image {
     width: 2rem;
 }
-
-
-
 
 .item-list {
     margin-top: 1rem;
@@ -282,29 +247,28 @@ form {
 
 .item-container-notice {
     text-align: center;
-
 }
-.search-container input[type=text]{
-    padding: .4rem .8rem;
+.search-container input[type='text'] {
+    padding: 0.4rem 0.8rem;
 }
 
-input[type=text] {
+input[type='text'] {
     border-radius: 50rem;
     border-color: var(--rarity-color-rare);
     border-style: solid;
 
-    transition: all .2s ease-in-out;
+    transition: all 0.2s ease-in-out;
 
     background-color: var(--background-button-color);
-    color: var(--text-color-body-white)
-}
-
-input[type=text]::placeholder{
     color: var(--text-color-body-white);
-    opacity: .6;
 }
 
-input[type=text]:focus {
+input[type='text']::placeholder {
+    color: var(--text-color-body-white);
+    opacity: 0.6;
+}
+
+input[type='text']:focus {
     border-color: var(--rarity-color-legendary);
     outline: none !important;
     outline-style: none;
@@ -312,27 +276,26 @@ input[type=text]:focus {
     box-shadow: none;
 }
 
-
-button[type=reset],
-button[type=submit] {
+button[type='reset'],
+button[type='submit'] {
     font-size: 1.4rem;
 }
 
-button[type=reset] > svg,
-button[type=submit] > svg {
+button[type='reset'] > svg,
+button[type='submit'] > svg {
     display: flex;
     justify-content: center;
     align-items: center;
 }
 
-button[type=reset] {
+button[type='reset'] {
     color: var(--rarity-color-exotic);
     appearance: none;
     background: none;
     border: none;
 }
 
-button[type=submit] {
+button[type='submit'] {
     color: var(--rarity-color-uncommon);
 
     appearance: none;
@@ -342,7 +305,7 @@ button[type=submit] {
 
 .item-container p > svg {
     color: var(--rarity-color-exotic);
-    padding-left: .2rem;
+    padding-left: 0.2rem;
 
     justify-content: center;
     align-items: center;
@@ -353,11 +316,11 @@ button[type=submit] {
 
 .list-enter-active,
 .list-leave-active {
-  transition: all 0.4s ease;
+    transition: all 0.4s ease;
 }
 .list-enter-from,
 .list-leave-to {
-  opacity: 0;
-  translate: 0 -100%
+    opacity: 0;
+    translate: 0 -100%;
 }
 </style>
