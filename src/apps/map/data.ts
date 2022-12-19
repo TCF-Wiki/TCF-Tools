80297e1002d6d7883a7cce64602e49b67163c78b
Stevnbak <kasper@stevnbak.dk>
2022-12-19 21:08:21 +0100

    map data fetch

--- src/apps/map/data.ts
+++ src/apps/map/data.ts
@@ -1,28 +1,29 @@
 let mapData: any = null;
+import {githubURL} from '../constants.json';
 
 export async function getMapData() {
     if (mapData) return mapData;
-    
+
     const loader = document.getElementById('loading');
 
     if (loader) {
         loader.style.display = 'block';
     }
 
-    const response = await fetch('https://raw.githubusercontent.com/Stevnbak/TCF-Information/main/mapData.json', {});
+    const response = await fetch(githubURL + 'mapData.json', {});
     const json = await response.json();
 
     if (loader) {
         loader.style.display = 'none';
     }
-    
+
     mapData = json;
     return json;
 }
 
 export async function getTierData() {
-    const response = await fetch('./tiers.json')
+    const response = await fetch('./tiers.json');
     const json = await response.json();
-    
+
     return json;
 }
