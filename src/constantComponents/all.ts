3537965bb959ba269b134aa68d02a1329a1c45ac
Stevn <kasper@stevnbak.dk>
2023-01-13 00:06:51 +0100

    Update all.ts

--- src/constantComponents/all.ts
+++ src/constantComponents/all.ts
@@ -1,12 +1,10 @@
-console.log("started loading");
-//loading
+//Loading
 export function doneLoading() {
-    console.log("done loading");
     let element = document.getElementById("loading");
     if (element) {
         element.remove();
     }
     document.querySelector("main")?.classList.add("loaded");
     //clean up the url bar
-    ///window.history.pushState({}, document.title, location.pathname.replace(".html", ""));
+    window.history.pushState({}, document.title, location.pathname.replace(".html", ""));
 }
