0b4624b786f7c1e8e9a42eafde392499a41fb38a
VeryGreatFrog <bvl46z@gmail.com>
2023-04-19 13:20:14 +0200

    more forge logic changes for 3.2.0

--- src/apps/forge/ValidItems.ts
+++ src/apps/forge/ValidItems.ts
@@ -40,36 +40,36 @@ export function getGearItems() {
     const shields = shieldData;
     const bags = backpackData;
 
-    let validHelmets: string[] = [];
-    let validShields: string[] = [];
-    let validBags: string[] = [];
+    let validHelmets: string[] = ["Helmet_04", "Helmet_05", "Helmet_Altered_03"];
+    let validShields: string[] = ["Shield_04", "Shield_05", "Shield_Altered_03"];
+    let validBags: string[] = ["Bag_04", "Bag_05", "Bag_Altered_03"];
 
-    for (let helmet in helmets) {
-        let items = helmets[helmet];
+    // for (let helmet in helmets) {
+    //     let items = helmets[helmet];
 
-        if (items["rarity"] === "Rare" || items["rarity"] === "Epic" || items["rarity"] === "Exotic") {
-            if (helmet.includes("Tactical") || helmet.includes("Restoration") || helmet.includes("Altered")) continue;
-            validHelmets.push(helmet);
-        }
-    }
+    //     if (items["rarity"] === "Epic" || items["rarity"] === "Exotic" || items["rarity"] === "Legendary") {
+    //         if (helmet.includes("Tactical") || helmet.includes("Restoration") || (items["inGameName"] != "Titan Forged Helmet" && helmet.includes("Altered"))) continue;
+    //         validHelmets.push(helmet);
+    //     }
+    // }
 
-    for (let shield in shields) {
-        let items = shields[shield];
+    // for (let shield in shields) {
+    //     let items = shields[shield];
 
-        if (items["rarity"] === "Rare" || items["rarity"] === "Epic" || items["rarity"] === "Exotic") {
-            if (shield.includes("Tactical") || shield.includes("Restoration") || shield.includes("Altered")) continue;
-            validShields.push(shield);
-        }
-    }
+    //     if (items["rarity"] === "Epic" || items["rarity"] === "Exotic" || items["rarity"] === "Legendary") {
+    //         if (shield.includes("Tactical") || shield.includes("Restoration") || shield.includes("Altered")) continue;
+    //         validShields.push(shield);
+    //     }
+    // }
 
-    for (let bag in bags) {
-        let items = bags[bag];
+    // for (let bag in bags) {
+    //     let items = bags[bag];
 
-        if (items["rarity"] === "Rare" || items["rarity"] === "Epic" || items["rarity"] === "Exotic") {
-            if (bag.includes("Altered")) continue;
-            validBags.push(bag);
-        }
-    }
+    //     if (items["rarity"] === "Epic" || items["rarity"] === "Exotic" || items["rarity"] === "Legendary") {
+    //         if (bag.includes("Altered")) continue;
+    //         validBags.push(bag);
+    //     }
+    // }
 
     validItems["gear"] = [...new Set(validHelmets), ...new Set(validShields), ...new Set(validBags)];
 }
